35f8b5120c640d9bfb49c92eab8ac951bc003b36
Andrew Whitaker <whitaker@cs.washington.edu>
2014-02-28 13:56:41 -0800

    Scaffolding for a join node

--- tree.py
+++ tree.py
@@ -16,6 +16,10 @@ class Node(object):
         """Set the partition attribute for the given node and all its descendents."""
 
     @abc.abstractmethod
+    def get_num_columns(self):
+        """Return number of columns."""
+
+    @abc.abstractmethod
     def get_output_size(self, color):
         """Estimate output size for the operator."""
 
@@ -39,8 +43,34 @@ class ScanNode(Node):
         assert color in range(self.num_columns)
         self.color = color
 
+    def get_num_columns(self):
+        return self.num_columns
+
     def get_output_size(self):
         return self.num_tuples
+
+class JoinNode(Node):
+    """Represent a join of two relations."""
+
+    def __init__(self, left, right, join_columns):
+        """
+        :type left: Node
+        :type right: Node
+        :type join_columns: set of join column two-tuples in sorted order
+        """
+        self.left = left
+        self.right = right
+        self.join_columns = join_columns
+
+    def get_costs(self):
+        pass
+
+    def get_num_columns(self):
+        return left.get_num_columns() + right.get_num_columns()
+
+    def get_output_size(self):
+        # XXX totally made up
+        return left.get_output_size() + right.get_output_size()
 
 if __name__ == '__main__':
     n = ScanNode({1,2})
